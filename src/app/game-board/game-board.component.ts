da164acfcdcbcfb874edfe45e1fdfde7d5eab33a
GreenM4x <greenmax02@gmail.com>
2023-10-23 21:01:40 +0200

    first layout design

---
+++ src/app/game-board/game-board.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-board',
+  templateUrl: './game-board.component.html',
+  styleUrls: ['./game-board.component.scss'],
+})
+export class GameBoardComponent {
+  optionOne: string = '7f75ed';
+  optionTwo: string = 'd00914';
+  optionThree: string = '989193';
+  optionFour: string = 'd73db0';
+}
