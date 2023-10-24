6572876f8fd9233d91675ebc1a91110c75bd101f
Maximilian Keller <greenmax02@gmail.com>
2023-10-24 10:40:05 +0200

    random Hex Generator

--- src/app/game-board/game-board.component.ts
+++ src/app/game-board/game-board.component.ts
@@ -1,15 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-game-board',
   templateUrl: './game-board.component.html',
   styleUrls: ['./game-board.component.scss'],
 })
-export class GameBoardComponent {
+export class GameBoardComponent implements OnInit {
   optionOne: string = '7f75ed';
   optionTwo: string = 'd00914';
   optionThree: string = '989193';
   optionFour: string = 'd73db0';
 
-  colorToGuess: string = '#d73db0';
+  colorToGuess: string = 'ffffff';
+
+  ngOnInit(): void {
+    this.GenerateRandomHex();
+  }
+  GenerateRandomHex() {
+    const letters = '0123456789ABCDEF';
+    let color = '';
+
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+
+    this.colorToGuess = color;
+  }
 }
