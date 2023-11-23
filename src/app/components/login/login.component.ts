843b0ce77b685d972cf378e7bd94e49a2e3915e1
GreenM4x <greenmax02@gmail.com>
2023-11-23 18:57:41 +0100

    not quite working sign up

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -14,6 +14,8 @@ import { FirebaseService } from '@core/services/firebase.service';
 export class LoginComponent implements OnInit {
 	isRegistered: boolean = true;
 	loginForm!: FormGroup;
+	public pwError: string = 'Password is reguired.';
+
 	constructor(
 		private formBuilder: FormBuilder,
 		private fbService: FirebaseService
@@ -23,18 +25,33 @@ export class LoginComponent implements OnInit {
 		this.loginForm = this.formBuilder.group({
 			username: ['', Validators.required],
 			password: ['', Validators.required],
+			repeatPassword: [''],
 		});
-	}
+		// Set the validation conditionally based on isRegistered value
+		this.loginForm
+			.get('repeatPassword')
+			.setValidators(this.isRegistered ? null : Validators.required);
 
-	onSubmit() {
-		if (this.loginForm.valid) {
-			this.signInWithEmail();
-		}
+		// Update the form status after changing validators
+		this.loginForm.get('repeatPassword').updateValueAndValidity();
 	}
 
 	toggleToRegister() {
 		this.isRegistered = !this.isRegistered;
 		console.log(this.isRegistered);
+
+		this.loginForm
+			.get('repeatPassword')
+			.setValidators(this.isRegistered ? null : Validators.required);
+		this.loginForm.get('repeatPassword').updateValueAndValidity();
+	}
+
+	onSubmit() {
+		if (this.loginForm.valid && this.isRegistered) {
+			this.signInWithEmail();
+		} else if (this.loginForm.valid && !this.isRegistered) {
+			this.signUpWithEmail();
+		}
 	}
 
 	signInWithEmail() {
@@ -43,6 +60,19 @@ export class LoginComponent implements OnInit {
 			this.loginForm.get('password').value
 		);
 		console.log('Signing in with email...');
+	}
+
+	signUpWithEmail() {
+		if (
+			this.loginForm.get('password').value ===
+			this.loginForm.get('repeatPassword').value
+		) {
+			console.log('Signing up with email...');
+		} else {
+			this.pwError = 'Password not match.';
+			this.loginForm.get('repeatPassword').touched;
+			this.loginForm.get('repeatPassword').hasError('required');
+		}
 	}
 
 	signInWithGoogle() {
