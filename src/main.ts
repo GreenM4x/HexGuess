9b2d261e8c846fc4fc4ed0ea2731ab5b837b2f2a
Lucas <lucaskeller98@gmail.com>
2023-11-27 12:57:42 +0100

    Bootstrap the app using standalone APIs

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,35 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { inject } from '@vercel/analytics';
- 
+
 inject();
 
-import { AppModule } from './app/app.module';
+import { AppComponent } from './app/app.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireAnalyticsModule } from '@angular/fire/compat/analytics';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { environment } from 'src/environments/environment';
+import { AngularFireModule } from '@angular/fire/compat';
+import { isDevMode, importProvidersFrom } from '@angular/core';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { AppRoutingModule } from './app/app-routing.module';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 
-platformBrowserDynamic()
-	.bootstrapModule(AppModule)
-	.catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+	providers: [
+		importProvidersFrom(
+			BrowserModule,
+			AppRoutingModule,
+			ServiceWorkerModule.register('ngsw-worker.js', {
+				enabled: !isDevMode(),
+				registrationStrategy: 'registerImmediately',
+			}),
+			AngularFireModule.initializeApp(environment.firebase),
+			AngularFirestoreModule,
+			AngularFireAnalyticsModule,
+			AngularFireAuthModule,
+			FormsModule,
+			CommonModule
+		),
+	],
+}).catch(err => console.error(err));
