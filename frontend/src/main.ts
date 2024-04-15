a7a346f9e949eb5b5002c632d2d20356c49997d7
Lucas <lucaskeller98@gmail.com>
2024-04-15 07:58:52 +0200

    disable service worker, add strip prefix

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -20,7 +20,7 @@ bootstrapApplication(AppComponent, {
 			BrowserModule,
 			AppRoutingModule,
 			ServiceWorkerModule.register('ngsw-worker.js', {
-				enabled: !isDevMode(),
+				enabled: false,
 				registrationStrategy: 'registerImmediately',
 			}),
 			AngularFireModule.initializeApp(environment.firebase),
